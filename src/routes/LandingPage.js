4957a20c9a0f000901e556cd54ca2df383596ab3
rafikaauda <rafika.auda4@gmail.com>
2023-10-26 12:26:57 +0700

    add routes landingpage

---
+++ src/routes/LandingPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+export default function landingPage() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/register'
+          element={<Register />}
+        />
+      </Routes>
+    </>
+  );
+}
