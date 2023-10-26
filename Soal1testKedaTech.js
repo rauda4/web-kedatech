329b7087f5845ef7195a881fc1ae5007726bcacd
rafikaauda <rafika.auda4@gmail.com>
2023-10-26 12:31:31 +0700

    rename soal 1

---
+++ Soal1testKedaTech.js
@@ -0,0 +1,55 @@
+1; // Buatlah fungsi dengan parameter berapa banyak angka yg akan ditampilkan secara looping, dimana jika angka tersebut habis dibagi 3 cetak "fish" jika habis dibagi 5 cetak "bash" dan jika habis dibagi 15 cetak "fish bash", cetak cukup salah satu dari kondisi tersebut, cetak menggunakan console log atau sejenisnya.
+console.log(' Soal No 1');
+const array1 = [1, 2, 3, 4, 5];
+
+array1.forEach((i) => {
+  let output = '';
+  if (i % 3 === 0) {
+    output += 'fish';
+  }
+  if (i % 5 === 0) {
+    output += 'bash';
+  }
+  if (output === '') {
+    output = i;
+  }
+  console.log(output);
+});
+
+2; // Fungsi untuk mengurutkan angka dari kecil ke besar
+console.log('Soal No 2');
+let arr = [];
+let asc = arr.sort((a, b) => {
+  return a - b;
+});
+// Fungsi untuk mengurutkan angka dari besar ke kecil
+let desc = arr.sort((a, b) => {
+  return b - a;
+});
+
+arr.push(1, 5, 4, 7, 2, 8, 9, 6);
+console.log('asc', asc);
+console.log('desc', desc);
+
+3; // Buatlah fungsi dengan input string jika string tersebut dibalik dan tetap sama dengan string yang diinputkan maka return true jika tidak return false
+console.log('Soal No 3');
+
+function isPalindrome(str) {
+  // Menghapus whitespace dan mengubah ke huruf kecil
+  str = str.toLowerCase();
+
+  // Membalikkan string
+  let reversed = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  // Mengecek apakah string asli dan yang dibalik sama
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log('tidak enak', isPalindrome('tidak enak')); // false
+console.log('auda adua', isPalindrome('auda adua')); // true
